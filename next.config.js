66e8c264fbff59991afdab7ec39f4aeec3ffeb4a
vijay2668 <vijay.rathod2668@gmail.com>
2023-12-12 13:23:03 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  ignoreBuildErrors: true,
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
